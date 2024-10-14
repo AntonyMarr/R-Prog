8389bce538f0488c8438252f6eead46e926c3f9e
AntonyMarr <amarrero22@usf.edu>
2024-10-14 19:57:34 -0400

    Rprog Module 7

---
+++ Rprog Module 7.R
@@ -0,0 +1,25 @@
+AIdf <- data.frame(ability.cov)
+head(AIdf[1:2])
+class(AIdf)
+str(ability.cov)
+class(ability.cov)
+
+
+#S3 Example
+a <- list(name = "Momo", species = "Grizzly", furColor = "Brown", dietType = "Omnivorous", age = 6)
+class(a) <- "Mammal"
+a
+
+#S4 Example 
+setClass("Mammal", slots = c(name = "character",
+                             species = "character",
+                             furColor = "character",
+                             dietType = "character",
+                             age = "numeric"))
+Momo <- new("Mammal",name = "Momo", species = "Grizzly", furColor = "Brown", dietType = "Omnivorous", age = 6)
+is(Momo)
+Momo@species    
+slot(Momo, "age")
+                           
+                            
+                            
